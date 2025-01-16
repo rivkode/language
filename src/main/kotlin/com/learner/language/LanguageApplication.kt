877767b2c3d27fbe5813c295732a9980f813f7d9
jonghun <jonghuncu@gmail.com>
2025-01-16 23:58:21 +0900

    init project

---
+++ src/main/kotlin/com/learner/language/LanguageApplication.kt
@@ -0,0 +1,11 @@
+package com.learner.language
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanguageApplication
+
+fun main(args: Array<String>) {
+    runApplication<LanguageApplication>(*args)
+}
---
+++ src/test/kotlin/com/learner/language/LanguageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learner.language
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanguageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
